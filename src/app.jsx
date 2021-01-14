977ba010a1b62c1ab7e00535c598ce5f41702a06
Bulent <eroglu.bulent98@gmail.com>
2021-01-14 11:58:16 +0000

    Updated todo

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import TodoList from './components/TodoList';
 
 // TODO:
-// Add a checklist item to the list
-// Remove a checklist item from the list
-// Mark checklist items and complete and incomplete
+// Add a checklist item to the list ✔️
+// Remove a checklist item from the list ✔️
+// Mark checklist items and complete and incomplete ✔️
+
+// Bonus:
+// Added Editing todo feature
 
 export default function app() {
   return (
